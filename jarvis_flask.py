62d9b4ddd2ea1b596764df0accd97752a4faa9a0
Brian Osburn <brianposburn@yahoo.com>
2018-08-26 16:32:11 -0500

    adding valid validation

--- jarvis_flask.py
+++ jarvis_flask.py
@@ -95,7 +95,7 @@ def message_receiver():
 @app.route('/heartbeat', methods=['POST'])
 def heartbeat():
 
-    test = request.form
+    test = request.data
     logging.debug("Payload = %s", test)
     validate_request(request)
     heartbeat_message = {'text':  'I\'m Alive'}
